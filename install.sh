b39ab66f74e9bf9c8fbb4985b82264598672a55c
Sergey Gopkalo <sevenmaxis@gmail.com>
2016-09-18 17:23:11 +0300

    install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+add_to_inputrc() {
+  file='/etc/inputrc'
+  string=$1
+
+  if grep -q "$string" "$file"; then
+    echo "found '$file' file with '$string' string"
+  else
+    echo "$string" >> $file
+  fi
+}
+
+add_to_inputrc 'set keymap vi-command'
+add_to_inputrc 'set editing-mode vi'
+
+. /etc/inputrc
